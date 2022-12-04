f9cadc39f377d3dd758a19725355b0806c8e4161
Jean Damascene Uwingeneye <MacbookPro@Jeans-MacBook-Pro.local>
2022-12-04 16:15:00 +0200

    Add Header Web Component

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 let users=[];
 let posts=[];
-let commets=[];
-let likes=[];
+let currentUser="";
 
-function User(username, password){
+function User(userID, username, password){
     this.username=username,
     this.password=password
 }
 
-function Post(author,title,image,content){
+function Post(postID, authorID,title,imageURL,content,likes,comments){
     this.author=author,
     this.title=title,
-    this.image=image,
-    this.content=content
+    this.imageURL=imageURL,
+    this.content=content,
+    this.likes=likes,
+    this.comments=comments
+    this.likes=0
 }
+
+
+// HEADER MENU CUSTOM ELEMENT
+class MyHeader extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+      <header>
+      <nav>
+          <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="contact.html">Contact</a></li>
+              <li><a href="about.html">About</a></li>
+              <li><a href="portfolio.html">Portfolio</a></li>
+              <li><a href="blog.html">Blog</a></li>
+              <li><a href="signin.html">Sign in</a></li>
+          </ul>
+      </nav>
+  </header>
+      `;
+    }
+  }
+      
+  customElements.define('my-header', MyHeader);
+
